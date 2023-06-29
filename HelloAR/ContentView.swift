245586525e0a62b557351ac6100263b353b758af
jiwook_han <jiwook.han.dev@gmail.com>
2023-06-29 14:30:07 +0900

    Text 생성

--- HelloAR/ContentView.swift
+++ HelloAR/ContentView.swift
@@ -21,39 +21,57 @@ struct ARViewContainer: UIViewRepresentable {
         let arView = ARView(frame: .zero)
         
         let anchor = AnchorEntity(plane: .horizontal)
-
-        let boxMaterial = SimpleMaterial(color: .systemPink, isMetallic: true)
-        let box = ModelEntity(
-            mesh: MeshResource.generateBox(size: 0.3, cornerRadius: 0.05),
-            materials: [boxMaterial]
+        
+        // 텍스트 생성
+        let textMaterial = SimpleMaterial(
+            color: .systemPink,
+            isMetallic: true
         )
         
-        let sphereMaterial = SimpleMaterial(color: .systemYellow, isMetallic: true)
-        let sphere = ModelEntity(
-            mesh: MeshResource.generateSphere(radius: 0.2),
-            materials: [sphereMaterial])
-        sphere.position = simd_make_float3(0.2, 0.4, 0.3)
+        let text = ModelEntity(mesh: MeshResource.generateText(
+            "Hello AR",
+            extrusionDepth: 0.03,
+            font: .systemFont(ofSize: 0.2),
+            containerFrame: .zero,
+            alignment: .center,
+            lineBreakMode: .byCharWrapping
+        ), materials: [textMaterial])
+       
+        anchor.addChild(text)
         
-        let planeMeterial = SimpleMaterial(color: .systemBlue, isMetallic: true)
-        let plane = ModelEntity(
-            mesh: MeshResource.generatePlane(
-                width: 0.5,
-                depth: 0.3
-            ),
-            materials: [planeMeterial]
-        )
-        plane.position = simd_make_float3(0, 0.7, 0)
+        // 객체 생성
+//        let boxMaterial = SimpleMaterial(color: .systemPink, isMetallic: true)
+//        let box = ModelEntity(
+//            mesh: MeshResource.generateBox(size: 0.3, cornerRadius: 0.05),
+//            materials: [boxMaterial]
+//        )
+//
+//        let sphereMaterial = SimpleMaterial(color: .systemYellow, isMetallic: true)
+//        let sphere = ModelEntity(
+//            mesh: MeshResource.generateSphere(radius: 0.2),
+//            materials: [sphereMaterial]
+//        )
+//        sphere.position = simd_make_float3(0.2, 0.4, 0.3)
+//
+//        let planeMeterial = SimpleMaterial(color: .systemBlue, isMetallic: true)
+//        let plane = ModelEntity(
+//            mesh: MeshResource.generatePlane(
+//                width: 0.5,
+//                depth: 0.3
+//            ),
+//            materials: [planeMeterial]
+//        )
+//        plane.position = simd_make_float3(0, 0.7, 0)
+//
+//        [box, sphere, plane].forEach {
+//            anchor.addChild($0)
+//        }
         
-        [box, sphere, plane].forEach {
-            anchor.addChild($0)
-        }
         arView.scene.anchors.append(anchor)
-        
         return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
-    
 }
 
 #if DEBUG
